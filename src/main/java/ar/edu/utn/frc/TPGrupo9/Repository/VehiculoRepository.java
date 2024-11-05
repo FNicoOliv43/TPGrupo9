c6f7ea6c2c7e06e0826c492b0a09d17fa61b4739
NicolasOlivera <95304@sistemas.frc.utn.edu.ar>
2024-11-05 16:18:43 -0300

    Labura Mauri.2

---
+++ src/main/java/ar/edu/utn/frc/TPGrupo9/Repository/VehiculoRepository.java
@@ -0,0 +1,7 @@
+package ar.edu.utn.frc.TPGrupo9.Repository;
+import ar.edu.utn.frc.TPGrupo9.Models.Vehiculo;
+import org.springframework.data.repository.CrudRepository ;
+
+public interface VehiculoRepository extends CrudRepository<Vehiculo, Integer > {
+
+}
